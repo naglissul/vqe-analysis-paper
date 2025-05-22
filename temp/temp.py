1fc93fb53651c1c0f37dd7ac63eec32f639b9266
naglissul <naglis1134@gmail.com>
2025-05-22 21:25:22 +0200

    Add a lot of data

--- temp/temp.py
+++ temp/temp.py
@@ -2,13 +2,15 @@
 
 from tequila.quantumchemistry import Molecule
 
-geomstring="Li 0.0 0.0 0.0\nH 0.0 0.0 1.6"
-mol = Molecule(geometry=geomstring)
+def make_hamiltonian(geomstring:str):
+    # geomstring="Li 0.0 0.0 0.0\nH 0.0 0.0 1.6"
+    mol = Molecule(geometry=geomstring, verbose=False)
 
-H = mol.make_hamiltonian()
-U = mol.make_ansatz(name="SPA") # or e.g. UpCCGSD
+    H = mol.make_hamiltonian()
+    return H
 
-print("Hamiltonian:")
-print(H)
-print("Ansatz:")
-print(U)
+def make_upccgsd(geomstring:str, order:int=1):
+    # geomstring="Li 0.0 0.0 0.0\nH 0.0 0.0 1.6"
+    mol = Molecule(geometry=geomstring, verbose=False)
+    U = mol.make_upccgsd_ansatz(order=order)
+    return U
